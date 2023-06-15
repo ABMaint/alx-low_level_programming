912f754cbc7c826561aa8c8e7fd06e7ab8b89ba1
ABMaint <bakkali.abdellah.enim@gmail.com>
2023-06-15 02:33:52 -0700

    5th fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main-101
- * desc
- *return 
- */
+* main-101
+* desc
+*return: Always 1 (Succes) 
+*/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
