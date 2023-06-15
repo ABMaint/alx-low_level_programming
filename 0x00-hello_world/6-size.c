b3a610c9efbdd76bf43506b069690e407fe6d58b
ABMaint <bakkali.abdellah.enim@gmail.com>
2023-06-15 03:01:02 -0700

    fjnf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 /**
-*main desc
-*/
+ * main - size
+ *  
+ * Description: ujodknfh
+ *
+ * Return: lñjsklg
+ */
+
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
@@ -9,6 +14,5 @@ printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-
-    return (0);
+return (0);
 }    
