4e80e9e4ae6834056cd53a019a03b57c76181b06
ABMaint <bakkali.abdellah.enim@gmail.com>
2023-06-19 02:45:40 -0700

    hbjf

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - sdhjk
+ *
+ * Description: hjgfhgf
+ *
+ * Return: fjgkj
+ */
+
+int main(void)
+{
+int i;
+
+i = 48;
+
+while (i < 58)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(44);
+putchar(32);
+}
+i++;
+putchar(10);
+return (0);
+}
